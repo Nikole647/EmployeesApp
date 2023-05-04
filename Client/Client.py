0601d032e9448438b2e2e92995f544ef5cb5f4bd
Nikole647 <nikole647@gmail.com>
2023-05-04 12:29:50 +0300

    update employees app

---
+++ Client/Client.py
@@ -0,0 +1,46 @@
+import requests
+
+isOpen = True
+
+
+def printMenu():
+    pass
+
+def addEmployee():
+    print("ss")
+def getEmployeeById():
+    pass
+def getEmployeeByName():
+    pass
+def getAllEmployees():
+    pass
+def updateEmployee():
+    pass
+def deleteEmployee():
+    pass
+def importEmployeesFromCsv():
+    pass
+def exportEmployeesToCsv():
+    pass
+
+
+functions = {
+    0: testServer,
+    1: addEmployee,
+    2: getEmployeeById,
+    3: getEmployeeByName,
+    4: getAllEmployees,
+    5: updateEmployee,
+    6: deleteEmployee,
+    7: importEmployeesFromCsv,
+    8: exportEmployeesToCsv,
+    9: exit
+}
+def main():
+    print("Welcome")
+    while(isOpen):
+        printMenu()
+        choice = int(input())
+
+if __name__ == "__main__":
+    main()
--- Server/Server.py
+++ Server/Server.py
@@ -1,9 +1,11 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask,render_template
 
-@app.route("/",methods=["GET"])
+
+app = Flask(__name__)
+print(__name__)
+@app.route("/", methods=["GET"])
 def index():
-    return "Hello world!!!"
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
-    #jgfvgfhvghv
