563d2e2d062e0e207cd2ea6ee6ba1f3969da0f32
Nikole647 <nikole647@gmail.com>
2023-05-07 11:40:01 +0300

    update client , adding employee page

--- Client/Client.py
+++ Client/Client.py
@@ -1,13 +1,38 @@
 import requests
+from Employee import Employee
+import json
+isOpen=True
+url="http://127.0.0.1:5000"
 
-isOpen = True
+def main():
+    print("Welcome")
+    while(isOpen):
+        printMenu()
+        choice = int(input())
+        functions[choice]() #סוגריים כדי לקרוא לזה כפונקציה מהמילון
 
+def testServer():
+    print(requests.get(url+"/test").text)
 
 def printMenu():
-    pass
+    print("0.Test server")
+    print("1.add employee")
+    print("2.get employee by Id")
+    print("3.get employee by name")
+    print("4.get all employee")
+    print("5.update employee")
+    print("6.delete employee")
+    print("7.import employee from csv")
+    print("8.export employees to csv")
+    print("9.exit")
+
 
 def addEmployee():
-    print("ss")
+    details=input("Please enter: (id, firs name, last name,gender, age,salary,mail)").split(",")
+    newEmp=Employee(int(details[0]),details[1],details[2],details[3],float(details[4]),float(details[5]),details[6])
+    newEmpJson = json.dumps(newEmp)
+    requests.post(url+'/addEmployee',json=newEmpJson)
+
 def getEmployeeById():
     pass
 def getEmployeeByName():
@@ -25,7 +50,7 @@ def exportEmployeesToCsv():
 
 
 functions = {
-    # 0: testServer,
+    0: testServer,
     1: addEmployee,
     2: getEmployeeById,
     3: getEmployeeByName,
@@ -36,11 +61,8 @@ functions = {
     8: exportEmployeesToCsv,
     9: exit
 }
-def main():
-    print("Welcome")
-    while(isOpen):
-        printMenu()
-        choice = int(input())
+
 
 if __name__ == "__main__":
-    main()
+    testServer()
+    #123,nicole,gurevich,22,fghhhhy,jnkjnjn
--- Client/Employee.py
+++ Client/Employee.py
@@ -1,61 +1,15 @@
 class Employee:
 
-    def __init__(self, name, gender, age, id, job, salary):
-        self.__name = name
+    def __init__(self, id,first_name,last_name, gender, age, salary,email):
+        self.__firstName = first_name
+        self.__lastName = last_name
         self.__gender = gender
         self.__age = age
         self.__id = id
-        self.__job = job
         self.__salary = salary
-
-
-    def __setName__(self, name):
-        self.__name = name
-
-
-    def __getName__(self):
-        return self.__name
-
-
-    def __setGender__(self, g):
-        self.__gender = g
-
-
-    def __getGender__(self):
-        return self.__gender
-
-
-    def __setAge__(self, age):
-        self.__age = age
-
-
-    def __getAge__(self):
-        return self.__age
-
-
-    def __setID__(self, id):
-        self.__id = id
-
-
-    def __getID__(self):
-        return self.__id
-
-
-    def __setJob__(self, job):
-        self.__job = job
-
-
-    def __getJob__(self):
-        return self.__job
-
-
-    def __setSalary__(self, salary):
-        self.__salary = salary
-
-
-    def __getSalary__(self):
-        return self.__salary
+        self.__email = email
 
 
     def __str__(self):
-        return "Name:{}, Age:{} ,ID: {},Job: {},Salary: {}".format(self.__name, self.__age, self.__id, self.__job,self.__salary)
+        return "ID: {},first name:{},last name{},Gender:{}, Age:{} ,Salary: {},email: {}".format(self.__id,self.first_name,self.__lastName, self.__gender,self.__age,self.__salary,self.__email)
+
